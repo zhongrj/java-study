76630d71bc47241e8c0fa5795d3bdbc3d94ef91c
rujie.zhong <329053269@qq.com>
2017-04-27 10:08:05 +0800

    加入缓存管理（暂用本地对象缓存）

---
+++ zzone/zzone-core/src/main/java/zrj/study/zzone/core/common/cache/CacheManage.java
@@ -0,0 +1,38 @@
+package zrj.study.zzone.core.common.cache;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * 缓存管理
+ *
+ * @author zhongrj
+ * @email 329053269@qq.com
+ * @date 2017/4/27
+ */
+public class CacheManage {
+
+    // --------------------------------------- 本地缓存配置 ---------------------------------------
+
+    private static final Map<String, Object> LOCAL_CACHE = new HashMap<>(); // 本地对象缓存
+
+    private static Map getCache() {
+        return LOCAL_CACHE;
+    }
+
+
+    // --------------------------------------- 公共方法 ---------------------------------------
+
+    public static Object get(String key) {
+        return getCache().get(key);
+    }
+
+    public static void put(String key, Object e) {
+        getCache().put(key, e);
+    }
+
+    public static void remove(String key) {
+        getCache().remove(key);
+    }
+
+}
