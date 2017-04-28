9f13fef44fac72e48490fa3bbc6a38f1ed142c09
rujie.zhong <329053269@qq.com>
2017-04-28 17:28:31 +0800

    数据库修改

--- zzone/zzone-core/src/zzone-core.sql
+++ zzone/zzone-core/src/zzone-core.sql
@@ -10,7 +10,7 @@ Target Server Type    : MYSQL
 Target Server Version : 50624
 File Encoding         : 65001
 
-Date: 2017-04-22 17:53:58
+Date: 2017-04-28 17:26:58
 */
 
 SET FOREIGN_KEY_CHECKS=0;
@@ -25,12 +25,8 @@ CREATE TABLE `core_session` (
   `create_date` datetime NOT NULL COMMENT '创建时间',
   `expire_date` datetime NOT NULL COMMENT '失效时间',
   PRIMARY KEY (`token`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
--- ----------------------------
--- Records of core_session
--- ----------------------------
-INSERT INTO `core_session` VALUES ('48ce6c4ee4bf4aa5902530c7a154a590', '1', '2017-04-22 15:00:09', '2017-04-29 15:00:10');
 
 -- ----------------------------
 -- Table structure for `core_user`
@@ -41,15 +37,23 @@ CREATE TABLE `core_user` (
   `account` varchar(50) NOT NULL COMMENT '用户名',
   `password` varchar(50) NOT NULL COMMENT '用户密码',
   `name` varchar(50) DEFAULT NULL COMMENT '用户昵称',
+  `mobile` varchar(50) DEFAULT NULL COMMENT '手机',
+  `email` varchar(50) DEFAULT NULL COMMENT '邮箱',
   `type` int(5) DEFAULT NULL COMMENT '用户类型 0普通用户 1会员用户',
   `status` int(5) DEFAULT NULL COMMENT '用户状态 0正常 1冻结 2黑名单',
   `create_date` datetime NOT NULL COMMENT '创建时间',
   `update_date` datetime NOT NULL COMMENT '更新时间',
   `del_flag` int(5) NOT NULL COMMENT '删除标识 0正常 1删除',
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 -- ----------------------------
 -- Records of core_user
 -- ----------------------------
-INSERT INTO `core_user` VALUES ('4e510ffb47904b179b7c805c568450a3', 'admin', 'admin', null, null, null, '2017-04-22 17:13:05', '2017-04-22 17:13:05', '0');
+INSERT INTO `core_user` VALUES ('0a27d82c021e456fb8b4f9e0fe343c1c', 'admin', 'admin', '超级管理员', '13660677179', '329063269@qq.com', '0', '0', '2017-04-27 16:10:40', '2017-04-27 16:10:40', '0');
+
+-- ----------------------------
+-- View structure for `core_user_basic`
+-- ----------------------------
+DROP VIEW IF EXISTS `core_user_basic`;
+CREATE ALGORITHM=UNDEFINED DEFINER=`root`@`localhost` SQL SECURITY DEFINER VIEW `core_user_basic` AS select `core_user`.`id` AS `id`,`core_user`.`account` AS `account`,`core_user`.`name` AS `name`,`core_user`.`mobile` AS `mobile`,`core_user`.`email` AS `email`,`core_user`.`type` AS `type`,`core_user`.`status` AS `status`,`core_user`.`del_flag` AS `del_flag` from `core_user`;
