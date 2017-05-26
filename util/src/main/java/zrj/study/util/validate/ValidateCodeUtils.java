946a013e8086b7af5b1ac4b08436d5483a9fa015
rujie.zhong <329053269@qq.com>
2017-05-26 17:56:47 +0800

    修改

--- util/src/main/java/zrj/study/util/validate/ValidateCodeUtils.java
+++ util/src/main/java/zrj/study/util/validate/ValidateCodeUtils.java
@@ -13,7 +13,7 @@ import java.util.Random;
  */
 public class ValidateCodeUtils {
 
-    private static final char[] CODE_ARRAY = {'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'J',
+    public static final char[] CODE_ARRAY = {'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'J',
             'K', 'M', 'N', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W',
             'X', 'Y', 'Z', '2', '3', '4', '5', '6', '7', '8', '9'};
     private static final int CODE_ARRAY_LENGTH = CODE_ARRAY.length;
