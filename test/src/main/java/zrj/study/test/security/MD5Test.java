82cc3c1cabce784df32eca13db91d3fe39d83f37
rujie.zhong <329053269@qq.com>
2017-09-04 09:23:36 +0800

    练习

---
+++ test/src/main/java/zrj/study/test/security/MD5Test.java
@@ -0,0 +1,12 @@
+package zrj.study.test.security;
+
+
+/**
+ * @author zhongrj
+ * @email 329053269@qq.com
+ * @date 2017/8/23
+ */
+public class MD5Test {
+    public static void main(String[] args) {
+    }
+}
